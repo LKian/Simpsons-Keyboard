9b80fc7f94ddaaf026967b3c8edcd6595683979a
Leah <leahkahwadjian@gmail.com>
2019-11-26 19:32:32 -0500

    cleanup and comments

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,24 +1,32 @@
-console.log("Hit it!");
+console.log("Welcome to Springfield!");
 
 function playAudio(e) {
+    // find the audio src with selected audio file
     const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
+
     const key = document.querySelector(`.key[data-key="${e.keyCode}"]`);
-    if (!audio) return;       // If there's no audio, returns nothing aka stops the function
-    audio.currentTime = 0;    // This resets audio back to 0
+    
+    // If there's no audio, return nothing aka stops the function
+    if (!audio) return;
+
+    // Resets audio back to 0
+    audio.currentTime = 0;
     audio.play();
     key.classList.add('highlight');
 }
 
 function removeTransition(e) {
-    if (e.propertyName !== 'transform') return // Skip it if not transform
+    // Skip it if not transform
+    if (e.propertyName !== 'transform') return
     this.classList.remove('highlight');
 }
 
-const keyboard = document.querySelectorAll('.key');  //look among entire keyboard
+const keyboard = document.querySelectorAll('.key');  // look among entire keyboard
 keyboard.forEach(key => key.addEventListener('transitionend', removeTransition))
-  
+
 window.addEventListener('keydown', playAudio) // Fx since it's reusable
 //prints key code in console as each key is pressed
-// window.addEventListener('keydown', function(e) {
-// 	console.log(e.keyCode); 
-// });
+window.addEventListener('keydown', function(e) {
+	console.log(e);
+	console.log(e.key, e.keyCode);
+});
